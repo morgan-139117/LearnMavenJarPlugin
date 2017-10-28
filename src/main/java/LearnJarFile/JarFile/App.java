25270c2a4666c19ead83522a3a0335da7efdca01
wangzi85 <wangzi@drop-tank.com>
2017-10-28 17:17:02 -0500

    end of today's session

--- src/main/java/LearnJarFile/JarFile/App.java
+++ src/main/java/LearnJarFile/JarFile/App.java
@@ -1,13 +1,28 @@
 package LearnJarFile.JarFile;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	static Logger LOGGER = Logger.getLogger(App.class.getName());
+	
     public static void main( String[] args )
     {
+    	
+    	LOGGER.log(Level.INFO, "haha");
+    	LOGGER.log(Level.INFO,null,"here"+"asd");
+    	LOGGER.log(Level.INFO, "abc  {0},{1},{2}",new String[]{"1","2","3"});
+    	LOGGER.info("this is for your info");
+    	LOGGER.config("config");
+    	LOGGER.fine("fine");
+    	LOGGER.severe("severe");
+    	LOGGER.finer("finder");
+    	LOGGER.warning("warning");
         System.out.println( "Hello World!" );
     }
 }
