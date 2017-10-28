30770efa3081d82cba2f73ae8bbb1347d492785a
wangzi85 <wangzi@drop-tank.com>
2017-10-28 15:27:18 -0500

    init

---
+++ src/main/java/LearnJarFile/JarFile/App.java
@@ -0,0 +1,13 @@
+package LearnJarFile.JarFile;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
